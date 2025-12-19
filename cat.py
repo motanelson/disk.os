3b1b8e6ea3cbc4a8fdfb66e0d923ee7d127c5734
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-12-19 18:19:44 +0000

    Delete cat.py

--- cat.py
+++
@@ -1,9 +0,0 @@
-import os
-print("\033[40;37m\ngive me a file list to cat separete by ,\n")
-i=input().strip()
-ii=i.split(",")
-for n in ii:
-    f1=open(n,"r")
-    r=f1.read()
-    f1.close()
-    print(r)

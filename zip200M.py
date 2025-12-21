ca476a4b53fa60bac2cf9161a47fbb045ca5674f
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-12-21 15:43:17 +0000

    Add files via upload

---
+++ zip200M.py
@@ -0,0 +1,25 @@
+import os
+k=1024
+m=k*k
+g=m*k
+t=g*k
+cc=b"\x00"
+print("\033c\033[40;37m\ngive me the output file name ? ")
+kk=b""
+mm=b""
+gg=b""
+def retb(c,value,n):
+    a=b""
+    a=c*value
+    f1=open(n,"wb")
+    f1.write(a)
+    f1.close()
+v=b""
+n=input()
+n=n.strip()
+nnn=200*m
+retb(cc,nnn,n)
+os.system("mkfs.fat $1".replace("$1",n))
+os.system("file $1".replace("$1",n))
+
+        
